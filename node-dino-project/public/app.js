3d77c2ddee6391ebec87d7c1f445d0ab47b66bef
Rich Rose <askrichardrose@gmail.com>
2020-08-16 21:53:53 +0100

    Phase 3 - read json, add html test data

--- node-dino-project/public/app.js
+++ node-dino-project/public/app.js
@@ -1,13 +1,40 @@
-//const json = require('./dino.json'); // Load the Dino data
 const dinoFormButton = document.querySelector('#btn'); // Btn element
+const columnOne = document.querySelector('#C1')
+const species = document.querySelector('#species')
+// Ref: https://gist.github.com/msmfsd/fca50ab095b795eb39739e8c4357a808
+let dinoData;
+  
+fetchAsync();
+
+// Load the information from the dino file
+async function fetchAsync() {
+  // Await the fetch execution
+  const data = await(fetch('data/dino.json'));
+  // Await the data being available
+  dinoData = await data.json();
+}
+
+// Get the information from the form
 
     // Create Dino Constructor
-//console.log(json["Dinos"][0].species);
+const dinoTemplate = {
+  diet: "xxx",
+  fact: "xxx",
+  height: 0,
+  species: "x",
+  weight: 0,
+  when: "x",
+  where: "x"
+}
+
 
     // Create Dino Objects
-
+// For each dinoData - create a Dinosaur object
 
     // Create Human Object
+// Create a Human object
+// Create a Pideon object
+
 
     // Use IIFE to get human data from form
 
@@ -24,11 +51,20 @@ const dinoFormButton = document.querySelector('#btn'); // Btn element
 
 
     // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
-function showGrid() {
 
+    // Use a Map to generate the Tile content
+
+        // Add tiles to DOM - render with IDs
+
+// Pass the object and complete the onscreen fields
+function showGridItem(){
+//  columnOne.textContent = "Anklyosaurus";
+  let dino = dinoData.Dinos[0].species;
+  columnOne.textContent = dino;
+  species.textContent = "Hello Changing Text";
 }
+
+
 
 
 // Remove form from screen
@@ -41,6 +77,10 @@ function hideDinoForm() {
 
 // On button click, prepare and display infographic
 dinoFormButton.addEventListener('click', (event) => {
-  //alert('Hide the form');
+  // Hide the Dino entry form
   hideDinoForm();
+
+  //species.textContent = "Hello Changing Text";
+  showGridItem();
+  // Process the infographic
 })
